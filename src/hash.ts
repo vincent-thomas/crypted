65811b525c545b9623fdf35be87c3ecf3069cc84
Vincent Thomas <beguine-badgast-0p@icloud.com>
2023-09-05 20:10:23 +0200

    feat: la till sha256 algo

--- src/hash.ts
+++ src/hash.ts
@@ -1,6 +1,9 @@
 import { hash, Algorithm, verify } from "@node-rs/argon2";
 import {BinaryToTextEncoding, createHash} from "node:crypto";
+
 const ALGORITHM = Algorithm.Argon2id;
+const DEFAULT_ENCODING: BinaryToTextEncoding = "base64"
+
 
 export const createPasswordHash = (password: string) =>
   hash(password, {
@@ -13,6 +16,8 @@ export const verifyPasswordHash = (hash: string, password: string) =>
   });
 
 
-export const md5Hash = (text: string, defaultEncoding: BinaryToTextEncoding = "base64") => createHash("md5").update(text).digest(defaultEncoding);
+export const md5Hash = (text: string, defaultEncoding: BinaryToTextEncoding = DEFAULT_ENCODING) => createHash("md5").update(text).digest(defaultEncoding);
+export const md5Verify = (textToCompare: string, hash: string, encoding: BinaryToTextEncoding = DEFAULT_ENCODING) => createHash("md5").update(textToCompare).digest(encoding) === hash;
 
-export const md5Verify = (textToCompare: string, hash: string, encoding: BinaryToTextEncoding = "base64") => createHash("md5").update(textToCompare).digest(encoding) === hash;
+export const sha256Hash = (text: string, defaultEncoding: BinaryToTextEncoding = DEFAULT_ENCODING) => createHash("sha256").update(text).digest(defaultEncoding);
+export const sha256Verify = (textToCompare: string, hash: string, encoding: BinaryToTextEncoding = DEFAULT_ENCODING) => createHash("sha256").update(textToCompare).digest(encoding) === hash;
