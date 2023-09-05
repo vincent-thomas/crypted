e3b4aacdd3e6fb4cfdaaf8c29cf58d454fda992b
Vincent Thomas <beguine-badgast-0p@icloud.com>
2023-09-05 20:06:29 +0200

    feat: la till md5 algo

--- src/hash.ts
+++ src/hash.ts
@@ -1,5 +1,5 @@
 import { hash, Algorithm, verify } from "@node-rs/argon2";
-
+import {BinaryToTextEncoding, createHash} from "node:crypto";
 const ALGORITHM = Algorithm.Argon2id;
 
 export const createPasswordHash = (password: string) =>
@@ -11,3 +11,8 @@ export const verifyPasswordHash = (hash: string, password: string) =>
   verify(hash, password, {
     algorithm: ALGORITHM
   });
+
+
+export const md5Hash = (text: string, defaultEncoding: BinaryToTextEncoding = "base64") => createHash("md5").update(text).digest(defaultEncoding);
+
+export const md5Verify = (textToCompare: string, hash: string, encoding: BinaryToTextEncoding = "base64") => createHash("md5").update(textToCompare).digest(encoding) === hash;
