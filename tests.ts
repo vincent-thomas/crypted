f3f094c21862bfca1dc8e5f59daf26cffe63dece
Vincent Thomas <beguine-badgast-0p@icloud.com>
2023-09-05 20:18:01 +0200

    test: la till för hash algos

--- tests.ts
+++ tests.ts
@@ -3,6 +3,7 @@ import { decrypt } from "./src/decrypt";
 import { encrypt } from "./src/encrypt";
 import { describe, it } from "node:test";
 import assert from "node:assert";
+import { createPasswordHash, md5Hash, md5Verify, sha256Hash, sha256Verify, verifyPasswordHash } from "./src";
 
 describe("Encryption & Decryption", () => {
   it("Should format string with the utf-8 encoding to a buffer", () => {
@@ -12,5 +13,30 @@ describe("Encryption & Decryption", () => {
     const TEST_DECRYPTED = decrypt(encrypted, KEY);
 
     assert(TEXT === TEST_DECRYPTED);
+  });
+});
+
+describe("Password Hashing and comparing", () => {
+  it("Should hash a password and compare it to the original", async () => {
+    const PASSWORD = "this is a password";
+    const hash = await createPasswordHash(PASSWORD);
+    const result = await verifyPasswordHash(hash, PASSWORD);
+    assert(result);
+  });
+});
+
+describe("Hashing and comparing", () => {
+  it("MD5 Algorithm", () => {
+    const TEXT = "this is a test";
+    const hash = md5Hash(TEXT);
+    const result = md5Verify(TEXT, hash);
+    assert(result);
+  });
+
+  it("SHA256 Algorithm", () => {
+    const TEXT = "this is a test";
+    const hash = sha256Hash(TEXT);
+    const result = sha256Verify(TEXT, hash);
+    assert(result);
   });
 });
